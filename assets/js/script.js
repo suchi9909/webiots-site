797e12d010ed0f9aeeadda189242c7b9497549c1
suchi <suchika.parekh@webiots.in>
2021-07-27 17:45:55 +0530

    vector demo added

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,6 +22,42 @@ $('.vertical-slider').slick({
 });
 
 
+$('.slide-5').slick({
+  dots: false,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 5,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 3500,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+});
+
+
 /*=====================
  Image to background js
  ==========================*/
