de837008817ac57306c85e3fde5314993423b8b1
suchi <suchika.parekh@webiots.in>
2021-08-05 18:14:15 +0530

    home section added

--- assets/js/script.js
+++ assets/js/script.js
@@ -57,6 +57,31 @@ $('.slide-5').slick({
   ]
 });
 
+$('.slide-3').slick({
+  dots: true,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: false,
+  autoplaySpeed: 3500,
+  responsive: [
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+});
 
 /*=====================
  Image to background js
