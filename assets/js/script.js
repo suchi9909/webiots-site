fac2fd346fc47ef1cbadaca46a298351198ac9a6
suchikaparekh <48489158+suchikaparekh@users.noreply.github.com>
2021-08-24 11:16:00 +0530

    latest

--- assets/js/script.js
+++ assets/js/script.js
@@ -68,7 +68,6 @@ $('.slide-3').slick({
   arrows: false,
   centerMode: true,
   centerPadding: '100px',
-  asNavFor: '.slider-nav',
   responsive: [
     {
       breakpoint: 600,
@@ -133,3 +132,44 @@ $(window).scroll(function(){
   if (scroll >= 100) sticky.addClass('fixed');
   else sticky.removeClass('fixed');
 });
+
+
+/*=====================
+ Swiper js
+ ==========================*/
+//  var swiper = new Swiper('.swiper-screenshots-container-1', {
+//   effect: 'coverflow',
+//   grabCursor: true,
+//   initialSlide: 2,
+//   centeredSlides: true,
+//   loop: true,
+//   spaceBetween: 0,
+//   slidesPerView: 2,
+//   pagination: false,
+//   coverflowEffect: {
+//       rotate: 0,
+//       stretch: 0,
+//       depth: 100,
+//       modifier: 1.43,
+//       slideShadows: false,
+//   },
+//   autoplay: {
+//       delay: 5000,
+//   },
+//   breakpoints: {
+//       360: {
+//           slidesPerView: 1,
+//           coverflowEffect: {
+//               depth: 50,
+//               modifier: 0,
+//           },
+//       },
+//       640: {
+//           slidesPerView: 3,
+//           coverflowEffect: {
+//               depth: 80,
+//               modifier: 0,
+//           },
+//       }
+//   }
+// });
