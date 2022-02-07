46accf330aaebb6a12de13f08122f7fa04b180e7
suchi9909 <suchika.parekh@webiots.in>
2022-02-07 22:50:50 +0530

    voxo add

--- appish/assets/js/progress-bar.js
+++
@@ -1,8 +0,0 @@
-(function ($) {
-    "use strict";
-    window.randomize = function () {
-        $(".ko-progress-circle").attr("data-progress", Math.floor(Math.random() * 100));
-    };
-    setTimeout(window.randomize, 200);
-    $(".ko-progress-circle").click(window.randomize);
-})(jQuery);
---
+++ voxo/front-end/assets/js/newsletter.js
@@ -0,0 +1,5 @@
+$(window).on('load', function () {
+    setTimeout(function () {
+        $('#newsletter').modal('show');
+    }, 500);
+});
